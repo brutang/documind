f0d549e49debf07c8fa857587e5ee5665ddd8128
Robin Colon <strider.chris@gmail.com>
2012-04-19 16:45:10 -0500

    working with gridbag layout

--- src/DocumindGUI.java
+++ src/DocumindGUI.java
@@ -1,6 +1,12 @@
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JScrollBar;
@@ -10,32 +16,71 @@ import javax.swing.JTextArea;
 public class DocumindGUI {
 
 	
-	private JFrame jFrame;
+	private JFrame frame;
+	private Container container;
 	private JTextArea documentArea;
 	private JScrollPane scrollPane;
+	private JButton getHighlightedButton;
 	
-	private static final int FRAME_HEIGHT = 800;
-	private static final int FRAME_WIDTH = 800;
+	private static final int FRAME_HEIGHT = 500;
+	private static final int FRAME_WIDTH = 500;
 
 	/**
 	 * @param args
 	 */
 	
 	public DocumindGUI() {
-		jFrame = new JFrame();
-		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jFrame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		container = frame.getContentPane();
 		
+		// Create the layout
+		GridBagLayout gbl = new GridBagLayout();
+		
+		// Set layout on container
+		container.setLayout(gbl);
+		
+		// Place a component at cell location (1,1)
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		gbc.fill = GridBagConstraints.BOTH;
+		
+		// Associate the gridbag constraints with the component
 		documentArea = new JTextArea();
 		documentArea.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		documentArea.setLineWrap(true);
 		scrollPane = new JScrollPane(documentArea);
-		jFrame.add(scrollPane);
+		gbl.setConstraints(scrollPane, gbc);
+		
+		gbc.gridx = 600;
+		gbc.gridy = 0;
+		gbc.weightx = 0;
+		gbc.weighty = 0;
+		gbc.fill = GridBagConstraints.NONE;
+		getHighlightedButton = new JButton("Get Highlighted Text");
+		gbl.setConstraints(getHighlightedButton, gbc);
+		
+		//add components to container
+		container.add(scrollPane);
+		container.add(getHighlightedButton);
+		
+		
+		
+		/*documentArea = new JTextArea();
+		documentArea.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		documentArea.setLineWrap(true);
+		scrollPane = new JScrollPane(documentArea);
+		frame.add(scrollPane);
+		frame.add(getHighlightedButton);*/
 	}
 	
 	public void openFile() throws IOException{
 		JFileChooser fileopen = new JFileChooser();
-		int ret = fileopen.showDialog(jFrame, "Select File");
+		int ret = fileopen.showDialog(frame, "Select File");
 		if(ret == JFileChooser.APPROVE_OPTION){
 			File file = fileopen.getSelectedFile();
 			FileReader reader = new FileReader(file.getPath());
@@ -49,8 +94,8 @@ public class DocumindGUI {
 	}
 	
 	public void viewGUI(){
-		//jFrame.pack();
-		jFrame.setVisible(true);
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
 	public static void main(String args[]){
